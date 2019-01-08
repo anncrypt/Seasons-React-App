8dfcb2f4ee62a56e2a436673e441721c7ab5b60b
Anna Ionova <echoespond@gmail.com>
2019-01-07 23:52:07 -0500

    make latitude coordinates render in the component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return <div>Hi There!</div>
+    this.state = {
+      lat: null,
+      // long: null
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
 }
 
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
-);
+); 
