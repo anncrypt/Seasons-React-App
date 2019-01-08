5a0a5ff8901d1c1f383e256f7f6a07e78b2d15dd
Anna Ionova <echoespond@gmail.com>
2019-01-08 12:54:26 -0500

    add state and some logic to the component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Disp</div>
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+ const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
+  
+  return <div>Blalal</div>
+  // <div>Latitude: {this.props.state.lat}</div>
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     lat: null,
+  //     errorMessage: ''
+  //   };
+  // }
 
+  // another way of setting state
+  state = { lat: null, errorMessage: '' };
+  
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
-    }
-    return <div>Loading!</div>
+    } else if (!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat} />;
+      } else {
+          return <div>Loading!</div>;
+        }
   }
 }
 
